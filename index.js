d434db66c3f397abc269a99961b38b38953d41b8
Nick Hehr <headhipster@hipsterbrown.com>
2015-07-16 15:20:03 -0400

    feat(unsynced-local-docs): initial implementation

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 var PouchDB = process.browser ? global.PouchDB : require('pouchdb')
 var API = require('pouchdb-hoodie-api')
 var Sync = require('pouchdb-hoodie-sync')
+var UnsyncedLocalDocs = require('pouchdb-hoodie-unsynced-local-docs')
 var merge = require('lodash.merge')
 var EventEmitter = require('events').EventEmitter
 
 PouchDB.plugin({
   hoodieApi: API.hoodieApi,
-  hoodieSync: Sync.hoodieSync
+  hoodieSync: Sync.hoodieSync,
+  unsyncedLocalDocs: UnsyncedLocalDocs.unsyncedLocalDocs
 })
 
 module.exports = Store
@@ -22,8 +24,27 @@ function Store (dbName, options) {
   var CustomPouchDB = PouchDB.defaults(options)
   var db = new CustomPouchDB(dbName)
   var emitter = new EventEmitter()
-  var api = merge(db.hoodieSync({remote: dbName + '-remote', emitter: emitter}), db.hoodieApi({emitter: emitter}))
+  var api = merge(
+    db.hoodieSync({remote: dbName + '-remote', emitter: emitter}),
+    db.hoodieApi({emitter: emitter}),
+    {
+      unsyncedLocalDocs: mapUnsyncedLocalIds.bind(db, db.unsyncedLocalDocs)
+    }
+  )
 
   return api
+}
+
+function mapUnsyncedLocalIds (unsyncedLocalDocs, options) {
+  return unsyncedLocalDocs.call(this, options)
+
+  .then(function (changedDocs) {
+    return changedDocs.map(function (doc) {
+      doc.id = doc._id
+      delete doc._id
+
+      return doc
+    })
+  })
 }
 
