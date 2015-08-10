021c23f5a2f8d7ec5b7538cc212793ef541d6b72
HipsterBrown <headhipster@hipsterbrown.com>
2015-08-09 21:04:57 -0400

    test(sync): check for hoodie objects from sync

--- tests/specs/sync.js
+++ tests/specs/sync.js
@@ -21,8 +21,70 @@ test('has "sync" methods', function (t) {
   t.is(typeof store.isConnected, 'function', 'had "isConnected" method')
 })
 
+test('store.push() returns hoodie objects', function (t) {
+  t.plan(3)
+
+  var store = new Store('test-db-push-return', merge({remote: 'test-db-push-return-remote'}, options))
+  var obj1 = {id: 'test1', foo: 'bar1'}
+  var obj2 = {id: 'test2', foo: 'bar2'}
+
+  store.add([obj1, obj2])
+
+  .then(function () {
+    return store.push()
+  })
+
+  .then(function (pushedObjs) {
+    t.is(pushedObjs.length, 2, '2 items returned')
+    t.is(pushedObjs[0].id, 'test1', 'returns hoodie object')
+    t.is(pushedObjs[1].foo, 'bar2', 'returns hoodie object')
+  })
+})
+
+test('store.push(docsOrIds) returns hoodie objects', function (t) {
+  t.plan(3)
+
+  var store = new Store('test-db-push-objects-return', merge({remote: 'test-db-push-objects-return-remote'}, options))
+  var obj1 = {id: 'test1', foo: 'bar1'}
+  var obj2 = {id: 'test2', foo: 'bar2'}
+
+  store.add([obj1, obj2])
+
+  .then(function (addedObjs) {
+    return store.push(addedObjs)
+  })
+
+  .then(function (pushedObjs) {
+    t.is(pushedObjs.length, 2, '2 items returned')
+    t.is(pushedObjs[0].id, 'test1', 'returns hoodie object')
+    t.is(pushedObjs[1].foo, 'bar2', 'returns hoodie object')
+  })
+
+  .catch(console.warn.bind(console))
+})
+
+test('store.sync() returns hoodie objects', function (t) {
+  t.plan(3)
+
+  var store = new Store('test-db-sync-return', merge({remote: 'test-db-sync-return-remote'}, options))
+  var obj1 = {id: 'test1', foo: 'bar1'}
+  var obj2 = {id: 'test2', foo: 'bar2'}
+
+  store.add([obj1, obj2])
+
+  .then(function () {
+    return store.sync()
+  })
+
+  .then(function (pushedObjs) {
+    t.is(pushedObjs.length, 2, '2 items returned')
+    t.is(pushedObjs[0].id, 'test1', 'returns hoodie object')
+    t.is(pushedObjs[1].foo, 'bar2', 'returns hoodie object')
+  })
+})
+
 test('store.on("push") for store.push()', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   var store = new Store('test-db-push', merge({remote: 'test-db-push-remote'}, options))
   var pushEvents = []
@@ -37,6 +99,7 @@ test('store.on("push") for store.push()', function (t) {
 
   .then(function () {
     t.is(pushEvents.length, 1, 'triggers 1 push event')
+    t.is(pushEvents[0].id, 'test', 'event passes object')
     t.is(pushEvents[0].foo, 'bar', 'event passes object')
   })
 })
@@ -74,7 +137,7 @@ test('api.off("push")', function (t) {
 })
 
 test('api.one("push")', function (t) {
-  t.plan(3)
+  t.plan(4)
 
   var store = new Store('test-db-push-one', merge({remote: 'test-db-push-one-remote'}, options))
   var pushEvents = []
@@ -92,6 +155,7 @@ test('api.one("push")', function (t) {
 
   .then(function () {
     t.is(pushEvents.length, 1, 'triggers 1 push event')
+    t.is(pushEvents[0].id, 'test1', 'event passes object')
     t.is(pushEvents[0].foo, 'bar1', 'event passes object')
   })
 
@@ -101,5 +165,22 @@ test('api.one("push")', function (t) {
 
   .then(function () {
     t.is(pushEvents.length, 1, 'triggers no second push event')
+  })
+})
+
+test('api.on("connect") for api.connect()', function (t) {
+  t.plan(1)
+
+  var store = new Store('test-db-on-connect', merge({remote: 'test-db-on-connect-remote'}, options))
+  var numConnectEvents = 0
+
+  store.on('connect', function () {
+    numConnectEvents += 1
+  })
+
+  store.connect()
+
+  .then(function () {
+    t.is(numConnectEvents, 1, '"connect" event triggered')
   })
 })
