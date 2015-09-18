4835a1955b1258c6f5323e1d529e8f74defb08c1
Gregor Martynus <gregor@martynus.net>
2015-09-18 13:16:26 +0200

    feat: initial version

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = Hoodie
+
+var Store = require('pouchdb-hoodie-store')
+var Account = require('account-client')
+var Task = function () {}
+
+var getState = require('./lib/get-state')
+var id = require('./lib/id').id
+
+function Hoodie (options) {
+  var state = getState(options)
+
+  var api = {}
+  api.id = id.bind(null, state)
+
+  var CustomStore = Store.defaults({ remoteBaseUrl: '/hoodie/store/api' })
+  var dbName = 'user/' + api.id()
+  api.store = new CustomStore(dbName)
+
+  api.account = new Account({ url: '/hoodie/account/api' })
+  api.task = new Task('/hoodie/task/api')
+
+  return api
+}
