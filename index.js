f47d619af6687b932d85ef37d6ab6313d3ba3c2e
Gregor Martynus <gregor@martynus.net>
2015-11-16 21:08:52 -0500

    feat: initial version

---
+++ index.js
@@ -0,0 +1,79 @@
+module.exports = Store
+
+var API = require('pouchdb-hoodie-api')
+var EventEmitter = require('events').EventEmitter
+var merge = require('lodash.merge')
+var PouchDB = global.PouchDB || require('pouchdb')
+var Sync = require('pouchdb-hoodie-sync')
+
+var hasLocalChanges = require('./lib/has-local-changes')
+var subscribeToInternalEvents = require('./lib/subscribe-to-internal-events')
+var subscribeToSyncEvents = require('./lib/subscribe-to-sync-events')
+var syncWrapper = require('./lib/sync-wrapper')
+var scoped = require('./lib/scoped/')
+
+PouchDB.plugin({
+  hoodieApi: API.hoodieApi,
+  hoodieSync: Sync.hoodieSync
+})
+
+function Store (dbName, options) {
+  if (!(this instanceof Store)) return new Store(dbName, options)
+  if (typeof dbName !== 'string') throw new Error('Must be a valid string.')
+
+  if (!options || (!options.remote && !options.remoteBaseUrl)) {
+    throw new Error('options.remote or options.remoteBaseUrl is required')
+  }
+
+  if (options.remoteBaseUrl) {
+    options.remoteBaseUrl = options.remoteBaseUrl.replace(/\/$/, '')
+    if (!options.remote) {
+      options.remote = dbName
+    }
+    if (!/^https?:\/\//.test(options.remote)) {
+      options.remote = (options.remoteBaseUrl + '/' + options.remote)
+    }
+  }
+
+  // we use a custom PouchDB constructor as we derive another PouchDB to
+  // interact with the remote store, and want it to inherit the options
+  var CustomPouchDB = PouchDB.defaults(options)
+  var db = new CustomPouchDB(dbName)
+  var emitter = new EventEmitter()
+  var remote = options.remote
+  var syncApi = db.hoodieSync({remote: remote})
+  var storeApi = db.hoodieApi({emitter: emitter})
+
+  var api = merge(
+    scoped.bind(null, storeApi),
+    storeApi,
+    {
+      hasLocalChanges: hasLocalChanges.bind(db),
+      push: syncWrapper.bind(syncApi, 'push'),
+      pull: syncWrapper.bind(syncApi, 'pull'),
+      sync: syncWrapper.bind(syncApi, 'sync'),
+      connect: syncApi.connect,
+      disconnect: syncApi.disconnect,
+      isConnected: syncApi.isConnected
+    }
+  )
+
+  subscribeToSyncEvents(syncApi, emitter)
+  subscribeToInternalEvents(emitter)
+
+  return api
+}
+
+Store.defaults = function (defaultOpts) {
+  function CustomStore (dbName, options) {
+    if (typeof dbName !== 'string') throw new Error('Must be a valid string.')
+    options = options || {}
+
+    options = merge({}, defaultOpts, options)
+
+    return Store(dbName, options)
+  }
+
+  return CustomStore
+}
+
