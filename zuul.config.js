82fb9090fc9c638c8f83b41b965bbde22f574651
Gregor Martynus <gregor@martynus.net>
2015-11-21 23:58:14 -0500

    test(zuul): fix order for scripts

--- zuul.config.js
+++ zuul.config.js
@@ -1,5 +1,5 @@
 module.exports = require('hoodie-zuul-config')
 module.exports.scripts = [
-  './node_modules/pouchdb/dist/pouchdb.memory.js',
-  './node_modules/pouchdb/dist/pouchdb.js'
+  './node_modules/pouchdb/dist/pouchdb.js',
+  './node_modules/pouchdb/dist/pouchdb.memory.js'
 ]
