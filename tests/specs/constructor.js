fe2bc9d44844b28074a8cb0ae8666304f7887c9d
Gregor Martynus <gregor@martynus.net>
2015-12-25 22:54:45 +0100

    test: use t.end() for sync tests
    * * *
    
    This commit was sponsored by &yet.
    
    Our friends at &yet have been supporters of Hoodie since its earliest days. <3
    And they have availability for new projects! We think they’re some of the best
    people you can hire anywhere, but you can read what other people have to say
    about them and their work here:
    
    https://andyet.com/case-studies/

--- tests/specs/constructor.js
+++ tests/specs/constructor.js
@@ -12,72 +12,72 @@ var options = process.browser ? {
 }
 
 test('new Store(db, options)', function (t) {
-  t.plan(3)
-
   var store = new Store('test-db', merge({remote: 'test-db-remote'}, options))
   var testDB = dbFactory('test-db')
 
   t.is(typeof store, 'function', 'is a constructor')
   t.ok(store.db, 'sets .db on instance')
   t.is(store.db._db_name, testDB._db_name, '.db is PouchDB object')
+
+  t.end()
 })
 
 test('Store(db, options) w/o new', function (t) {
-  t.plan(3)
-
   var store = Store('test-db', merge({remote: 'test-db-remote'}, options))
   var testDB = dbFactory('test-db')
 
   t.is(typeof store, 'function', 'is a constructor')
   t.ok(store.db, 'sets .db on instance')
   t.is(store.db._db_name, testDB._db_name, '.db is PouchDB object')
+
+  t.end()
 })
 
 test('new Store()', function (t) {
-  t.plan(1)
-
   t.throws(Store, 'throws error')
+
+  t.end()
 })
 
 test('new Store(dbName)', function (t) {
-  t.plan(1)
-
   t.throws(Store.bind(null, 'db'), 'throws error')
+
+  t.end()
 })
 
 test('new Store("name", {remote: "othername"})', function (t) {
-  t.plan(1)
-
   var store = new Store('name', merge({ remote: 'othername' }, options))
   t.is(store.db.__opts.remote, 'othername', 'sets remote name to "othername"')
+
+  t.end()
 })
 
 test('new Store("name", {remoteBaseUrl: "https://example.com"})', function (t) {
-  t.plan(1)
-
   var store = new Store('name', merge({ remoteBaseUrl: 'https://my.cou.ch' }, options))
   t.is(store.db.__opts.remote, 'https://my.cou.ch/name', 'sets remote name to "https://my.cou.ch/name"')
+
+  t.end()
 })
 
 test('remoteBaseUrl with trailing /', function (t) {
-  t.plan(1)
-
   var store = new Store('name', merge({ remoteBaseUrl: 'https://my.cou.ch/' }, options))
   t.is(store.db.__opts.remote, 'https://my.cou.ch/name', 'sets remote name without double //')
+
+  t.end()
 })
 
 test('new Store("name", {remote: "othername", remoteBaseUrl: "https://example.com"})', function (t) {
-  t.plan(1)
-
   var store = new Store('name', merge({ remote: 'othername', remoteBaseUrl: 'https://my.cou.ch' }, options))
   t.is(store.db.__opts.remote, 'https://my.cou.ch/othername', 'sets remote name to "https://my.cou.ch/othername"')
+
+  t.end()
 })
 
 test('new Store("name", {remote: "othername", remoteBaseUrl: "https://example.com"})', function (t) {
-  t.plan(1)
-
   var store = new Store('name', merge({ remote: 'https://my.other.cou.ch/name', remoteBaseUrl: 'https://my.cou.ch' }, options))
   t.is(store.db.__opts.remote, 'https://my.other.cou.ch/name', 'sets remote name to "https://my.cou.ch/othername"')
+
+  t.end()
 })
 
 test('constructs a store object without options.adapter / options.db', function (t) {
@@ -92,24 +92,24 @@ test('constructs a store object without options.adapter / options.db', function 
 })
 
 test('Store.defaults({remote})', function (t) {
-  t.plan(2)
-
   var CustomStore = Store.defaults(merge({remote: 'test-db-custom-remote'}, options))
   var store = new CustomStore('test-db-custom')
 
   t.throws(CustomStore, 'throws without argumens')
   t.is(store.db.__opts.remote, 'test-db-custom-remote', 'sets remote name')
+
+  t.end()
 })
 
 test('Store.defaults({remoteBaseUrl})', function (t) {
-  t.plan(2)
-
   var CustomStore = Store.defaults(merge({remoteBaseUrl: 'http://example.com'}, options))
   var store = new CustomStore('test-db-custom')
   var store2 = new CustomStore('test-db-custom2')
 
   t.is(store.db.__opts.remote, 'http://example.com/test-db-custom', 'sets remote name')
   t.is(store2.db.__opts.remote, 'http://example.com/test-db-custom2', 'sets remote name correcly on multiple instances')
+
+  t.end()
 })
 
 function cleanupDb (store, t) {
