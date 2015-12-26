3340ce15274b0a1f1bada596c5b9f7a24ff96bac
Gregor Martynus <gregor@martynus.net>
2015-12-26 01:56:18 +0100

    feat: hoodie.id is now hoodie.account.id
    BREAKING CHANGE:
    hoodie.id is now hoodie.account.id
    
    * * *
    
    This commit was sponsored by &yet.
    
    Our friends at &yet have been supporters of Hoodie since its earliest days. <3
    And they have availability for new projects! We think they’re some of the best
    people you can hire anywhere, but you can read what other people have to say
    about them and their work here:
    
    https://andyet.com/case-studies/
    
    * * *
    
    This commit was sponsored by &yet.
    
    Our friends at &yet have been supporters of Hoodie since its earliest days. <3
    And they have availability for new projects! We think they’re some of the best
    people you can hire anywhere, but you can read what other people have to say
    about them and their work here:
    
    https://andyet.com/case-studies/

--- index.js
+++ index.js
@@ -7,31 +7,48 @@ var ConnectionStatus = require('hoodie-client-connection-status')
 var Log = require('hoodie-client-log')
 
 var getState = require('./lib/get-state')
-var id = require('./lib/id')
 
 function Hoodie (options) {
   var state = getState(options)
 
   var api = {
-    get id () {
-      return id.get(state)
-    },
     get url () {
       return state.url + '/hoodie'
     }
   }
 
-  var CustomStore = Store.defaults({ remoteBaseUrl: api.url + '/store/api' })
-  var dbName = 'user/' + api.id
-  api.store = new CustomStore(dbName)
+  var account = new Account({ url: api.url + '/account/api' })
 
-  api.account = new Account({ url: api.url + '/account/api' })
-  api.task = new Task('/hoodie/task/api')
+  var CustomStore = Store.defaults({
+    remoteBaseUrl: api.url + '/store/api',
+    ajax: function () {
+      var session = api.account.get('session')
+      if (!session) {
+        return
+      }
+
+      return {
+        headers: {
+          authorization: 'Bearer ' + session.id
+        }
+      }
+    }
+  })
+  var dbName = 'user/' + account.id
+  var store = new CustomStore(dbName)
+
+  var task = new Task('/hoodie/task/api')
+
+  // core modules
+  api.account = account
+  api.store = store
+  api.task = task
   api.request = require('./lib/request').bind(this, state)
   api.connectionStatus = new ConnectionStatus(api.url)
   api.log = new Log('hoodie')
   api.plugin = require('./lib/plugin')
 
+  // events
   api.on = require('./lib/events').on.bind(this, state)
   api.one = require('./lib/events').one.bind(this, state)
   api.off = require('./lib/events').off.bind(this, state)
