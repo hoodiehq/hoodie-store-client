69877bb19dab9a7fe6a8846d1348dcd1b7143d51
Jan Pawellek <git@janpawellek.de>
2016-03-02 16:26:15 +0100

    test: added tests for events.js

--- tests/specs/events.js
+++ tests/specs/events.js
@@ -8,11 +8,43 @@ test('has "on" method', function (t) {
   t.is(typeof hoodie.on, 'function', 'has method')
 })
 
+test('"on" without listener', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.throws(function () {
+    hoodie.on('account:signin')
+  }, /listener must be a function/, 'throws exception when listener is not a function')
+})
+
+test('"on" returns an object', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.is(typeof hoodie.on('account:signin', function () {}), 'object', 'returns an object')
+})
+
 test('has "one" method', function (t) {
   t.plan(1)
 
   var hoodie = new Hoodie()
   t.is(typeof hoodie.one, 'function', 'has method')
+})
+
+test('"one" returns an object', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.is(typeof hoodie.one('account:signin', function () {}), 'object', 'returns an object')
+})
+
+test('"one" without listener', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.throws(function () {
+    hoodie.one('account:signin')
+  }, /listener must be a function/, 'throws exception when listener is not a function')
 })
 
 test('has "off" method', function (t) {
@@ -22,9 +54,32 @@ test('has "off" method', function (t) {
   t.is(typeof hoodie.off, 'function', 'has method')
 })
 
+test('"off" returns an object', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.is(typeof hoodie.off('account:signin', function () {}), 'object', 'returns an object')
+})
+
+test('"off" without listener', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.throws(function () {
+    hoodie.off('account:signin')
+  }, /listener must be a function/, 'throws exception when listener is not a function')
+})
+
 test('has "trigger" method', function (t) {
   t.plan(1)
 
   var hoodie = new Hoodie()
   t.is(typeof hoodie.trigger, 'function', 'has method')
+})
+
+test('"trigger" returns an object', function (t) {
+  t.plan(1)
+
+  var hoodie = new Hoodie()
+  t.is(typeof hoodie.trigger('account:signin'), 'object', 'returns an object')
 })
