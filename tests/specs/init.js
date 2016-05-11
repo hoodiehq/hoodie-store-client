f32f3857e71d4ecb355a006b603b72354e82dcc5
mccullagh <mccullagh@users.noreply.github.com>
2016-05-11 05:07:16 +0200

    test(init.js): 100% code coverage. (#85)

--- tests/specs/init.js
+++ tests/specs/init.js
@@ -197,3 +197,162 @@ test('hoodie.store initialization without session', function (t) {
   var storeAjaxParam = CustomStoreMock.lastCall.args[1]
   t.is(storeAjaxParam.ajax(), undefined, 'no authorization header without session')
 })
+
+test('"hoodie.store.push" is called on "pre:signout"', function (t) {
+  t.plan(2)
+
+  var hoodie = {
+    account: {
+      id: 0,
+      on: simple.stub(),
+      isSignedIn: simple.stub()
+    },
+    store: {
+      push: function () {
+        t.pass('store.push called on "signout"')
+      }
+    },
+    connectionStatus: {
+      on: simple.stub()
+    }
+  }
+
+  init(hoodie)
+  var signOutHandler = findEventHandler(hoodie.account.on.calls, 'pre:signout')
+  var hooks = []
+  signOutHandler({hooks: hooks})
+  t.is(hooks.length, 1, 'one pre:signout hook registered')
+  hooks[0]()
+})
+
+test('"hoodie.store.*" is *not* called when "hoodie.account.isSignedIn()" returns "false"', function (t) {
+  t.plan(2)
+
+  var hoodie = {
+    account: {
+      id: 0,
+      on: simple.stub(),
+      isSignedIn: simple.stub().returnWith(false)
+    },
+    store: {
+      disconnect: simple.stub(),
+      connect: simple.stub()
+    },
+    connectionStatus: {
+      on: function (name, listener) {
+        listener()
+      }
+    }
+  }
+
+  init(hoodie)
+  t.is(hoodie.store.disconnect.callCount, 0, 'calls hoodie store.disconnect zero')
+  t.is(hoodie.store.connect.callCount, 0, 'calls hoodie store.connect zero')
+})
+
+test('"hoodie.store.*" is called on "disconnect" and "connect"', function (t) {
+  t.plan(2)
+
+  var hoodie = {
+    account: {
+      id: 0,
+      on: simple.stub(),
+      isSignedIn: simple.stub().returnWith(true)
+    },
+    store: {
+      disconnect: function (options) {
+        t.pass('store.disconnect called on "disconnect"')
+      },
+      connect: function (options) {
+        t.pass('store.connect called on "connect"')
+      }
+    },
+    connectionStatus: {
+      on: function (name, listener) {
+        listener()
+      },
+      ok: false // else connect is called a second time
+    }
+  }
+
+  init(hoodie)
+})
+
+test('"dataFromAccountBeforeSignin" nulled on "signin"', function (t) {
+  t.plan(3)
+
+  var existingObjects = [{id: 'foo', createdBy: 'accountid1'}]
+  var hoodie = {
+    account: {
+      id: 'accountid1',
+      on: simple.stub(),
+      isSignedIn: simple.stub()
+    },
+    store: {
+      findAll: function () {
+        return Promise.resolve(existingObjects)
+      },
+      connect: function () {
+        t.pass('store.connect called on "signin"')
+      }
+    },
+    connectionStatus: {
+      on: simple.stub()
+    }
+  }
+
+  init(hoodie)
+  var preSignInHandler = findEventHandler(hoodie.account.on.calls, 'pre:signin')
+  var postSignInHandler = findEventHandler(hoodie.account.on.calls, 'post:signin')
+
+  var preHooks = []
+  preSignInHandler({hooks: preHooks})
+
+  var postHooks = []
+  postSignInHandler({hooks: postHooks})
+
+  t.is(preHooks.length, 1, 'one pre:signin hook registered')
+  t.is(postHooks.length, 1, 'one post:signin hook registered')
+  preHooks[0]()
+  .then(postHooks[0])
+})
+
+test('"signin" with Error', function (t) {
+  t.plan(2)
+
+  var existingObjects = [{id: 'foo', createdBy: 'accountid1'}]
+  var hoodie = {
+    account: {
+      id: 'accountid1',
+      on: simple.stub(),
+      isSignedIn: simple.stub()
+    },
+    store: {
+      findAll: function () {
+        return Promise.resolve(existingObjects)
+      },
+      reset: simple.stub().rejectWith(new Error('Ooops'))
+    },
+    connectionStatus: {
+      on: simple.stub()
+    }
+  }
+
+  init(hoodie)
+  var preSignInHandler = findEventHandler(hoodie.account.on.calls, 'pre:signin')
+  var postSignInHandler = findEventHandler(hoodie.account.on.calls, 'post:signin')
+
+  var preHooks = []
+  preSignInHandler({hooks: preHooks})
+
+  var postHooks = []
+  postSignInHandler({hooks: postHooks})
+
+  t.is(preHooks.length, 1, 'one pre:signin hook registered')
+  t.is(postHooks.length, 1, 'one post:signin hook registered')
+  preHooks[0]().then(function () {
+    // simulate new account.id
+    hoodie.account.id = 'accountid2'
+  })
+  .then(postHooks[0])
+})
