f00e75858cf08cc7e14e1039a7fa4b1193be7a8c
Gregor <gregor@martynus.net>
2017-06-20 10:14:54 -0700

    test: scoped store API works after reset

--- tests/integration/reset.js
+++ tests/integration/reset.js
@@ -25,3 +25,53 @@ test('scoped store calls should work after reset (hoodiehq/hoodie#612)', functio
 
   .catch(t.error)
 })
+
+test('scoped store APIs work after reset (hoodiehq/hoodie-store-client#149)', function (t) {
+  t.plan(1)
+
+  var store = new Store('test-db-reset', {
+    PouchDB: PouchDB,
+    remote: 'test-db-reset'
+  })
+
+  var itemStore = store.withIdPrefix('item/')
+
+  store.reset()
+
+  .then(function () {
+    return itemStore.findAll()
+  })
+
+  .then(function () {
+    t.pass('ok')
+  })
+
+  .catch(t.error)
+})
+
+test('events work on scoped APIs after reset', function (t) {
+  t.plan(2)
+
+  var store = new Store('test-db-reset', {
+    PouchDB: PouchDB,
+    remote: 'test-db-reset'
+  })
+
+  var itemStore = store.withIdPrefix('item/')
+
+  itemStore.on('add', function (doc) {
+    t.is(doc.foo, 'bar')
+  })
+
+  store.reset()
+
+  .then(function () {
+    return itemStore.add({foo: 'bar'})
+  })
+
+  .then(function () {
+    t.pass('ok')
+  })
+
+  .catch(t.error)
+})
